c978ca5a734cb396d6375ae6f1c04a97b9f32ace
xanv <xan.vong@gmail.com>
2014-08-01 18:00:10 -0500

    Add base template and stylesheet
    Add a sample base template and style.css, as well as admin and 404/500 pages

--- painindex/settings/settings_base.py
+++ painindex/settings/settings_base.py
@@ -90,3 +90,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# May need to fiddle with this
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'painindex/static/painindex')
