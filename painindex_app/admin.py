e2bf7b09834e90a3dd0e61f9e533043b9b79cc59
xanv <xan.vong@gmail.com>
2014-08-23 15:51:59 -0500

    Register PainProfile model with admin

--- painindex_app/admin.py
+++ painindex_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.db import models
 from django import forms
-from painindex_app.models import PainTag, PainSource, PainReport
+from painindex_app.models import PainTag, PainSource, PainReport, PainProfile
 
 
 admin.site.register(PainTag)
 admin.site.register(PainSource)
 admin.site.register(PainReport)
+admin.site.register(PainProfile)
