d414c703dac5ac7033fcb6d67a2ebf2d67359fff
xanv <xan.vong@gmail.com>
2014-09-10 11:33:51 -0500

    Analyze scraped tweets

--- painindex_app/fixtures/scripts/tweetsentiment1.py
+++ painindex_app/fixtures/scripts/tweetsentiment1.py
@@ -1,5 +1,5 @@
 """This is a first pass at gauging pain intensity from tweets.
-We use a simplistic approach, motivated in part by tweet sentiment analysis: 
+We use a simplistic approach. 
 1) Each tweet is associated with a pain source and thus a pain intensity.
 2) For each word, average the intensities of every tweet the word appears in;
     call this the word_sentiment.
@@ -21,7 +21,7 @@ We use a simplistic approach, motivated in part by tweet sentiment analysis:
     This gives us a conversion from pain_sentiment to predicted intensity.
 
 Note that this approach is similar to the tweet sentiment approach where
-we start with sentiment ratings for a smal list of individual words, and use
+we start with sentiment ratings for a small list of individual words, and use
 them to derive sentiment ratings for other words and tweets as a whole.
 In our case, we start with sentiment ratings for whole tweets and use them
 to derive sentiment ratings for individual words and thus tweets not in our
@@ -32,17 +32,17 @@ Advantages and disadvantages:
 1) This is a good first approach, quick and easy. It may perform well.
 
 2) A disadvantage is that it won't necessarily generalize to non-tweet writing.
-For example, consider an article that discusses the painfulness of a yellowjacket
-sting but then goes on to talk about lots of other unrelated stuff.
+For example, consider an article that discusses the painfulness of a 
+yellowjacket sting but then goes on to talk about lots of other unrelated stuff.
 The predicted intensity will be diluted. We can correct for this by trying
 to extract just the discussion around the yellowjacket's sting. In fact,
 we can incorporate such samples into the training data.
 
-3) A deeper problem is that it doesn't allow us to express more complex relationships
-between the appearance of words and predicted pain levels. If the word "ouch" only
-appears in tweets with intensity 4, then its mere presence should predict a 4
-regardless of the other words. A properly-designed regression would take this
-into account, whereas here we just weight everything equally.
+3) A deeper problem is that it doesn't allow us to express more complex
+relationships between the appearance of words and predicted pain levels. If the
+word "ouch" only appears in tweets with intensity 4, then its mere presence
+should predict a 4 regardless of the other words. A properly-designed regression
+would take this into account, whereas here we just weight everything equally.
 
 Refinements: 
 Use a regression.
@@ -57,10 +57,16 @@ from collections import defaultdict
 
 
 def main():
-    with open('../data/outputs/twitter_results.txt') as json_tweets:
-        tweets = json.load(json_tweets)
-    with open('../data/outputs/pains.txt') as json_pains:
+    with open('../data/outputs/pains_20140910.txt') as json_pains:
         pains = json.load(json_pains)
+
+    # We just use the scraped tweets. There are so few from the API,
+    # and it wouldn't be good to use both.
+
+    # with open('../data/outputs/twitter_results_20140910.txt') as json_tweets:
+    #     tweets = json.load(json_tweets)
+    with open('../data/outputs/twitter_results_scraped_20140910.txt') as json_tweets_scraped:
+        tweets = json.load(json_tweets_scraped)
 
     tweets_words = get_tweets_words(tweets)
     word_sentiments = find_word_sentiments(tweets_words, pains)
