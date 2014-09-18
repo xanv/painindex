1519d9dc2f483671aee0f92252dd839a4d7af9c3
xanv <xan.vong@gmail.com>
2014-09-18 06:07:10 -0500

    Add About page TemplateView

--- painindex_app/urls.py
+++ painindex_app/urls.py
@@ -5,9 +5,9 @@ from painindex_app import views
 
 urlpatterns = patterns('',
     url(r'^$', views.homepage, name='homepage'),
+    url(r'^about/$', TemplateView.as_view(template_name='painindex_app/about.html'), name='about'),
     url(r'^painsource/(?P<painsource_id>\d+)/$', views.painsource_detail, name='painsource_detail'),
     # url(r'^painreport/new/$', views.painreport_form, name='painreport'),
-    url(r'^painreport/new/$', views.PainReportView.as_view(), name='painreport'),
-    
+    url(r'^painreport/new/$', views.PainReportView.as_view(), name='painreport'),    
             
 )
