94f1d4af970076005d7271c1e91b8b9a148a018d
xanv <xan.vong@gmail.com>
2014-09-21 16:52:58 -0500

    Add basic heroku production settings

--- painindex/settings/settings_prod.py
+++ painindex/settings/settings_prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from painindex.settings.settings_base import *
 
 # This file is NOT part of our repo. It contains sensitive settings like secret key
@@ -9,21 +10,15 @@ from env import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-
 # Apps used specifically for production
 INSTALLED_APPS += (
     'gunicorn',
 )
 
-
-# Configure production emails.
-
-
 # These people will get error emails in production
 ADMINS = (
     ('Xan', 'xan.vong@gmail.com'),
 )
-
 
 # Set this to match the domains of the production site.
 ALLOWED_HOSTS = [
@@ -32,8 +27,21 @@ ALLOWED_HOSTS = [
     'localhost'
 ]
 
-# Define place my static files will be collected and served from.
-# See https://docs.djangoproject.com/en/1.6/ref/settings/#std:setting-STATIC_ROOT
-# STATIC_ROOT = ""
+###################
+# Heroku settings #
+###################
 
-# MEDIA_ROOT = ""
+# See https://devcenter.heroku.com/articles/getting-started-with-django
+
+DATABASES['default'] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Heroku instructions allow all hosts. 
+# If I have a problem, try this.
+# ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
