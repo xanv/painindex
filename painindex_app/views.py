3947305060a4890212af5593c9f40371020d75a2
xanv <xan.vong@gmail.com>
2014-09-21 22:12:18 -0500

    find some bugs

--- painindex_app/views.py
+++ painindex_app/views.py
@@ -10,10 +10,10 @@ from django.http import HttpResponse
 def homepage(request):
     find_bugs = [PainSource.objects.select_random_in_range(i - 0.5, i + 0.5) 
         for i in range(10,0,-1)]
-    bugs = filter(None, find_bugs)
-    highlighted_bug = random.choice(bugs)
+    # bugs = filter(None, find_bugs)
+    # highlighted_bug = random.choice(bugs)
 
-    content = {"find_bugs": find_bugs, "highlighted_bug": highlighted_bug}
+    # content = {"find_bugs": find_bugs, "highlighted_bug": highlighted_bug}
     # return render(request, 'painindex_app/homepage.html', content)
     return HttpResponse("hi")
 
