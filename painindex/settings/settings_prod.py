e93fc1f295b4d6bb26ae6fa0f0b04a14871ccc48
xanv <xan.vong@gmail.com>
2014-09-21 21:33:50 -0500

    Set DATABASES['default']

--- painindex/settings/settings_prod.py
+++ painindex/settings/settings_prod.py
@@ -47,7 +47,7 @@ ALLOWED_HOSTS = [
 
 # This DATABASES default is just for running foreman locally.
 # I import it in env instead. 
-# DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 # DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
