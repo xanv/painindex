4b08a96b2e07fcd063fd96ba516835ecb47c98a5
xanv <xan.vong@gmail.com>
2014-09-21 22:32:47 -0500

    Add comment on IndexError

--- painindex_app/views.py
+++ painindex_app/views.py
@@ -11,6 +11,7 @@ def homepage(request):
     find_bugs = [PainSource.objects.select_random_in_range(i - 0.5, i + 0.5) 
         for i in range(10,0,-1)]
     bugs = filter(None, find_bugs)
+    # This throws an IndexError if bugs is empty.
     highlighted_bug = random.choice(bugs)
 
     content = {"find_bugs": find_bugs, "highlighted_bug": highlighted_bug}
