a28856f51ca5eaa769f103b4c5bd1d0b45082bc8
xanv <xan.vong@gmail.com>
2014-09-21 22:07:43 -0500

    make homepage simple

--- painindex_app/views.py
+++ painindex_app/views.py
@@ -4,16 +4,18 @@ from django.views.generic.edit import CreateView
 from django.core.urlresolvers import reverse_lazy
 from painindex_app.models import PainSource
 from painindex_app.forms import PainReportForm
+from django.http import HttpResponse
 
 
 def homepage(request):
-    find_bugs = [PainSource.objects.select_random_in_range(i - 0.5, i + 0.5) 
-        for i in range(10,0,-1)]
-    bugs = filter(None, find_bugs)
-    highlighted_bug = random.choice(bugs)
+    # find_bugs = [PainSource.objects.select_random_in_range(i - 0.5, i + 0.5) 
+    #     for i in range(10,0,-1)]
+    # bugs = filter(None, find_bugs)
+    # highlighted_bug = random.choice(bugs)
 
-    content = {"find_bugs": find_bugs, "highlighted_bug": highlighted_bug}
-    return render(request, 'painindex_app/homepage.html', content)
+    # content = {"find_bugs": find_bugs, "highlighted_bug": highlighted_bug}
+    # return render(request, 'painindex_app/homepage.html', content)
+    return HttpResponse("hi")
 
 def painsource_detail(request, painsource_id):
     painsource = get_object_or_404(PainSource, pk=painsource_id)
